d399ae04a721920f8aea044c13a35bb0ae0e2c83
akhil841 <akhilschoolemail@gmail.com>
2023-02-12 21:23:09 -0800

    Added working version of US2

--- app/src/main/java/com/example/socialcompass/CompassActivity.java
+++ app/src/main/java/com/example/socialcompass/CompassActivity.java
@@ -15,51 +15,68 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class CompassActivity extends AppCompatActivity {
-    //hashmap storing the names and coordinates of locations
+    //arrays holding:
+    //-coordinates passed from intent
+    //-labels passed from intent
+    //-IDs for the markers
+    //-IDs for the marker label TextViews
     public String[] locationsCoordinates;
     public String[] locationsLabels;
     public int[] locationPointerIDs;
 
     public int[] labelPointerIDs;
-
+    //The number of locations that can be shown on the compass
     public int numOfLocations = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        //initialize arrays
         locationsCoordinates = new String[numOfLocations];
         locationsLabels = new String[numOfLocations];
         locationPointerIDs = new int[numOfLocations];
         labelPointerIDs = new int[numOfLocations];
+        //fill arrays with data from intents
         loadLocationCoordinates();
         loadLocationLabels();
         loadLocationPointerIDs();
         loadLabelPointerIDs();
-
+        //set TextViews to label text
         for (int i = 0; i < numOfLocations; i++)
             ((TextView) findViewById(labelPointerIDs[i])).setText(locationsLabels[i]);
 
+        //main loop:
+        //initializes the locations of the markers and labels
+        //when we add rotation, this will be done continuously
         for (int i = 0; i < numOfLocations; i++)
         {
+            //if no coordinates are sent, don't draw the marker or its label
+            if (locationsCoordinates[i] == "default")
+            {
+                findViewById(locationPointerIDs[i]).setVisibility(View.INVISIBLE);
+                findViewById(labelPointerIDs[i]).setVisibility(View.INVISIBLE);
+                continue;
+            }
+            //compute angle and update marker and label
             float angle = compassCalculateAngle("0,0", locationsCoordinates[i]);
             updatePointer(locationPointerIDs[i], angle);
             updateLabelPointer(i);
         }
     }
 
+    //fills location array
     public void loadLocationCoordinates(){
-
-        //SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         Bundle extra = getIntent().getExtras();
         String[] locationNames = {"myHomeLocation","familyLocation","friendLocation"};
 
         for(int i = 0; i < locationNames.length; i++){
-            locationsCoordinates[i] = extra.getString(locationNames[i], "default" + locationNames[i]);
+            locationsCoordinates[i] = extra.getString(locationNames[i], "default");
         }
 
 
     }
 
+    //fill label array
     public void loadLocationLabels(){
 
         //SharedPreferences preferences = getPreferences(MODE_PRIVATE);
@@ -72,6 +89,7 @@ public class CompassActivity extends AppCompatActivity {
 
     }
 
+    //fill marker ID array
     public void loadLocationPointerIDs()
     {
         locationPointerIDs[0] = R.id.Marker1;
@@ -79,12 +97,16 @@ public class CompassActivity extends AppCompatActivity {
         locationPointerIDs[2] = R.id.Marker3;
     }
 
+    //fill TextView ID array
     public void loadLabelPointerIDs()
     {
         labelPointerIDs[0] = R.id.Label1;
         labelPointerIDs[1] = R.id.Label2;
         labelPointerIDs[2] = R.id.Label3;
     }
+
+    //compute angle at which marker and TextView must be placed
+    //takes user location and object location as input
     public float compassCalculateAngle(String userLocation, String objectLocation)
     {
         //split user coords into x and y
@@ -98,6 +120,7 @@ public class CompassActivity extends AppCompatActivity {
         return -((float) Math.toDegrees((float) Math.atan(yDiff/xDiff) + (float) ((xDiff < 0) ? Math.PI : 0)) - 90);
     }
 
+    //updates marker pointer with required angle
     public void updatePointer(int markerId, float angle){
         ImageView marker = findViewById(markerId);
         ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) marker.getLayoutParams();
@@ -105,27 +128,17 @@ public class CompassActivity extends AppCompatActivity {
         marker.setLayoutParams(layoutParams);
     }
 
+    //updates TextView label pointer with required angle
     public void updateLabelPointer(int index)
     {
         TextView label = findViewById(labelPointerIDs[index]);
         ImageView marker = findViewById(locationPointerIDs[index]);
-        float xCoord = marker.getX();
-        float yCoord = marker.getY() + 50;
-        yCoord = (yCoord > 0) ? yCoord : -yCoord;
-        label.setX(xCoord);
-        label.setY(yCoord);
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) marker.getLayoutParams();
+        layoutParams.circleAngle -= 5;
+        label.setLayoutParams(layoutParams);
     }
 
     public void goBackClicked(View view) {
         finish();
-    }
-
-    public void buttonPressed(View view) {
-        ImageView marker = findViewById(locationPointerIDs[0]);
-        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) marker.getLayoutParams();
-        layoutParams.circleAngle += 10;
-        marker.setLayoutParams(layoutParams);
-
-
     }
 }
--- app/src/main/java/com/example/socialcompass/LocationService.java
+++
@@ -1,5 +0,0 @@
-package com.example.socialcompass;
-
-public class LocationService {
-
-}

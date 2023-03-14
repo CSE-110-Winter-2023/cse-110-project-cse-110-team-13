c0bedb6b49f194fe11b5e3752d279ca09cbf9b96
CNsquared <aoshima@ucsd.edu>
2023-03-13 20:08:19 -0700

    Truncates the length of the labels if over 10 chars

--- app/src/main/java/com/example/socialcompass/CompassActivity.java
+++ app/src/main/java/com/example/socialcompass/CompassActivity.java
@@ -28,7 +28,7 @@ public class CompassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
-        this.builder = new MarkerBuilder(getApplicationContext());
+        this.builder = new MarkerBuilder();
         // fill arrays with data from intents
         loadFriendsFromUIDs();
         for (int i = 0; i < friends.size(); i++) {
